5f2a0dfb2aa8e0403b9bf34dd46771c1f6ee76de
Alexander Almström <info@alexanderalmstrom.com>
2025-12-01 10:31:27 +0100

    Fix vercel url

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -16,7 +16,7 @@ export function getBaseURL() {
   }
 
   if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+    return process.env.VERCEL_URL;
   }
 
   return `http://localhost:${process.env.PORT ?? 3000}`;
